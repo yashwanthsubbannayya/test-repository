32e6014d58b8cdfb6bbc09658139f68148065d3d
Yashwanth <yashwanth.subbannayya@gmail.com>
2019-10-24 13:38:18 +0200

    edited

--- example.py
+++ example.py
@@ -8,4 +8,9 @@ def test_add():
 
 
 def subtract(a, b):
-    return a + b
+    return a + b  # <--- fix this in step 8
+
+
+# uncomment the following test in step 5
+#def test_subtract():
+#    assert subtract(2, 3) == -1
